37eb4b6218ba5d743169195aac18ebf50534007e
object-ions <moshikoatia@gmail.com>
2023-09-08 23:57:10 -0400

    add CalorieTracker, Meal and Workout classes

--- js/app.js
+++ js/app.js
@@ -21,5 +21,15 @@ class Meal {
   constructor(name, calories) {
     // Add random id with hexadecimal number minus '0.'
     this.id = Math.random().toString(36).slice(2);
+    this.name = name;
+    this.calories = calories
+  }
+}
+class Workout {
+  constructor(name, calories) {
+    // Add random id with hexadecimal number minus '0.'
+    this.id = Math.random().toString(36).slice(2);
+    this.name = name;
+    this.calories = calories
   }
 }
